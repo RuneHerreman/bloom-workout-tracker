6fa48a1e7df86784ba50b6ecc688f04e61e34298
RuneHerreman <herreman.rune@gmail.com>
2026-01-27 00:11:04 +0100

    Added discover label

--- frontend/bloom/src/components/home/HeaderComponent.tsx
+++ frontend/bloom/src/components/home/HeaderComponent.tsx
@@ -4,6 +4,10 @@ function HeaderComponent() {
     return (
         <header id="home-header">
             <h1>Lift Track Eat Repeat.</h1>
+            <div id="discover">
+                <img src="/media/arrow_down.png" alt="arrow down"/>
+                <p>DISCOVER</p>
+            </div>
         </header>
     );
 }
