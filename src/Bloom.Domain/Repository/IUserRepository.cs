6676aba571d44a71b10887b4fdbfdbee54b04e84
RuneHerreman <herreman.rune@gmail.com>
2026-02-04 12:43:39 +0100

    Add tasks to interface

---
+++ src/Bloom.Domain/Repository/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Bloom.Domain.Entity;
+
+namespace Bloom.Domain.Repository;
+
+public interface IUserRepository
+{
+    Task<User> GetUser(Guid id);
+    Task RegisterUser(User user);
+    Task LoginUser(string email, string password);
+}
