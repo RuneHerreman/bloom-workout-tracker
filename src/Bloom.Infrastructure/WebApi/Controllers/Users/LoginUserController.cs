d675db8858b8d5aad38acb1e0b2aef4b54d6351f
RuneHerreman <herreman.rune@gmail.com>
2026-04-07 16:54:56 +0200

    Refactor LoginUserController: update Invoke method signature and throw NotImplementedException

--- src/Bloom.Infrastructure/WebApi/Controllers/Users/LoginUserController.cs
+++ src/Bloom.Infrastructure/WebApi/Controllers/Users/LoginUserController.cs
@@ -1,17 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bloom.Infrastructure.WebApi.Controllers.Users;
 
 public static class LoginUserController
 {
-    // public async Task<ActionResult> Invoke(
-    //     [FromBody] LoginCommand command
-    // )
-    // {
-    //     var result = await _mediator.Send(command);
-    //     if (result.IsSuccess)
-    //         return Ok(new { token = result.Value });
-    //     
-    //     return Unauthorized(new { error = result.Errors.FirstOrDefault() });
-    // }
+    public static async Task<Results<Ok, UnauthorizedHttpResult>> Invoke(
+        [FromBody] string command
+    )
+    {
+        throw new NotImplementedException();
+    }
 }
