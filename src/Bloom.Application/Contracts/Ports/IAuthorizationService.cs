8c1438ba5ea8db9e40cbfb1f4306d943815d4135
RuneHerreman <herreman.rune@gmail.com>
2026-05-04 19:53:31 +0200

    add auth port

---
+++ src/Bloom.Application/Contracts/Ports/IAuthorizationService.cs
@@ -0,0 +1,10 @@
+﻿using Bloom.Domain.Users;
+
+namespace Bloom.Application.Contracts.Ports;
+
+public interface IAuthorizationService
+{
+    string GenerateToken(UserId userId, string email, string username);
+    bool ValidateToken(string token);
+    UserId? GetUserIdFromToken(string token);
+}
