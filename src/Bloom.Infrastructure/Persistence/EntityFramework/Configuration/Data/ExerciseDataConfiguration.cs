76ffa1624560cffba00a5d6bd7551272bc456a5b
RuneHerreman <herreman.rune@gmail.com>
2026-05-05 16:43:03 +0200

    add query to ef

--- src/Bloom.Infrastructure/Persistence/EntityFramework/Configuration/Data/ExerciseDataConfiguration.cs
+++ src/Bloom.Infrastructure/Persistence/EntityFramework/Configuration/Data/ExerciseDataConfiguration.cs
@@ -1,22 +1,21 @@
-using Bloom.Domain.Exercises;
+﻿using Bloom.Application.Contracts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Bloom.Infrastructure.Persistence.EntityFramework.Configuration.Domain;
+namespace Bloom.Infrastructure.Persistence.EntityFramework.Configuration.Data;
 
-public class ExerciseConfiguration : IEntityTypeConfiguration<Exercise>
+public class ExerciseDataConfiguration: IEntityTypeConfiguration<ExerciseData>
 {
-    public void Configure(EntityTypeBuilder<Exercise> builder)
+    public void Configure(EntityTypeBuilder<ExerciseData> builder)
     {
         builder.ToTable("Exercises");
-
+        
         builder.HasKey(e => e.Id);
         builder.Property(e => e.Id).ValueGeneratedNever();
-
+        
         builder.Property(e => e.Name).IsRequired();
         builder.Property(e => e.Description).IsRequired();
         builder.Property(e => e.Type).IsRequired().HasConversion<string>();
-
         builder.OwnsMany(e => e.TargetMuscles, tmBuilder =>
         {
             tmBuilder.ToJson();
--- src/Bloom.Infrastructure/Persistence/EntityFramework/Configuration/QueryDbContext.cs
+++ src/Bloom.Infrastructure/Persistence/EntityFramework/Configuration/QueryDbContext.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bloom.Application.Contracts;
+using Bloom.Infrastructure.Persistence.EntityFramework.Configuration.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Bloom.Infrastructure.Persistence.EntityFramework.Configuration;
 
 public class QueryDbContext: DbContext
 {
+    public DbSet<ExerciseData> Exercises { get; set; }
+    
+    
     protected QueryDbContext()
     {
         ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
@@ -12,7 +17,9 @@ public class QueryDbContext: DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // modelBuilder
+        modelBuilder
+            .ApplyConfiguration(new ExerciseDataConfiguration());
+        
         base.OnModelCreating(modelBuilder);
     }
 }
