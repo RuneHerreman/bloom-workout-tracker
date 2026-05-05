6d1b905deb638b84c69f683af85f1824c23d76bb
RuneHerreman <herreman.rune@gmail.com>
2026-05-05 16:50:58 +0200

    change seeder, add query

---
+++ src/Bloom.Infrastructure/Persistence/EntityFramework/Queries/SearchExerciseCatalogQuery.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using Bloom.Application.Contracts;
+using Bloom.Application.Contracts.Ports;
+using Bloom.Infrastructure.Persistence.EntityFramework.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloom.Infrastructure.Persistence.EntityFramework.Queries;
+
+public class SearchExerciseCatalogQuery(QueryDbContext context): ISearchExerciseCatalogQuery
+{
+    public async Task<IReadOnlyList<ExerciseData>> Fetch(Expression<Func<ExerciseData, bool>> filter)
+    {
+        return await context.Exercises
+            .Where(filter)
+            .OrderBy(e => e.TargetMuscles)
+            .ToListAsync();
+    }
+}
