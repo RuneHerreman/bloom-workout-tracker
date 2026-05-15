142e93369b59ab32ca704bcff8f81898961b17bf
RuneHerreman <herreman.rune@gmail.com>
2026-05-16 00:53:06 +0200

    remove todo

--- src/Bloom.Application/Contracts/Ports/IUnitOfWork.cs
+++ src/Bloom.Application/Contracts/Ports/IUnitOfWork.cs
@@ -4,7 +4,6 @@ namespace Bloom.Application.Contracts.Ports;
 
 public interface IUnitOfWork
 {
-    // TODO: should this IUnitOfWork extend IDisposable or IAsyncDisposable?
     Task Do();
     Task Save<TRepository>(IAggregateRoot aggregateRoot) where TRepository : IRepository;
     TRepository Repo<TRepository>() where TRepository : IRepository;
